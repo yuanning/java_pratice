e5c0be48dce00d7b0564efed3c66528f0dfc8e77
yn <yuanning@sit.com.cn>
2020-06-01 20:44:49 +0800

    thread.sleep

---
+++ src/main/java/thread/ThreadSleep.java
@@ -0,0 +1,20 @@
+/**
+ * @Title:
+ * @Package thread
+ * @Description: // TODO
+ * @author: Administrator
+ * @date: 2020-06-01 16:05
+ * @version V1.0
+ * @Copyright: Copyright (C) 2020 Sailing, Inc. All rights reserved.
+ */
+package thread;
+
+/**
+ * @ClassName: ThreadSleep
+ * @Description: // TODO 
+ * @author: Administrator
+ * @date: 2020-06-01 16:05 
+ * @Copyright: Copyright (C) 2020 Sailing, Inc. All rights reserved.
+ */
+public class ThreadSleep {
+}
