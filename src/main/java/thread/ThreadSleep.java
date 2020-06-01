0ddfe327d8537345a5edcfbce82d57e0cee68d8f
yn <yuanning@sit.com.cn>
2020-06-01 20:48:54 +0800

    thread.sleep

--- src/main/java/thread/ThreadSleep.java
+++ src/main/java/thread/ThreadSleep.java
@@ -16,5 +16,45 @@ package thread;
  * @date: 2020-06-01 16:05 
  * @Copyright: Copyright (C) 2020 Sailing, Inc. All rights reserved.
  */
-public class ThreadSleep {
+public class ThreadSleep implements Runnable {
+
+    @Override
+    public void run()  {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Thread currentThread = Thread.currentThread();
+                Thread.State state = currentThread.getState();
+                System.out.println(state.name());
+                System.out.println(currentThread.getName());
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+//        while (true) {
+//            try {
+//                Thread currentThread = Thread.currentThread();
+//                Thread.State state = currentThread.getState();
+//                System.out.println(state.name());
+//                System.out.println(currentThread.getName());
+//                Thread.sleep(5000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//                Thread.currentThread().interrupt();
+//                return;
+//            } finally {
+//                System.out.println("finally");
+//            }
+//        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadSleep threadSleep = new ThreadSleep();
+        Thread t1 = new Thread(threadSleep);
+        t1.start();
+        Thread.sleep(3000);
+        t1.interrupt();
+    }
+
 }
